8987b0bd4cd7cec75f4894d977f44ad0b6f5a842
Specter Panther <neuralnexuslab@hotmail.com>
2026-04-04 13:53:31 +0800

    Update aimc.js

--- script/aimc.js
+++ script/aimc.js
@@ -1,17 +1,16 @@
 function cal() {
-  const rsl = document.getElementById("rsl");
-  rsl.innerHTML = "";
+  document.getElementById("rsl").innerHTML = "";
   const equ = document.getElementById("eqt").value;
   const resl = eval(equ);
   let i = 0;
   const interval = setInterval(() => {
     if (i < resl) {
-      rsl.innerText += (i + 1);
+      document.getElementById("rsl").innerHTML += (i + 1) + " ";
       i++;
     } else {
       clearInterval(interval);
-      rsl.innerHTML += "<hr>";
-      rsl.innerText += "The result is " + resl;
+      document.getElementById("rsl").innerHTML += "<hr>";
+      document.getElementById("rsl").innerText += "The result is " + resl;
     }
   }, 333);
 }
